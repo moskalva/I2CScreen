100e3dd765916851d345bdd2966031026c33b61d
moskalva <vadym.moskalenko.outlook.com>
2023-03-19 14:50:42 +0100

    add text support via System.Drawing

--- My.I2C.Screen/Program.cs
+++ My.I2C.Screen/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Device.I2c;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -16,6 +19,7 @@ namespace My.E2C.Screen
 {
     class Program
     {
+        private static readonly Logger Log = Logger.Get();
 
         public static UInt16 RaspberryPiDefaultI2EBusAddress = 1;
         static void Main(string[] args)
@@ -26,23 +30,69 @@ namespace My.E2C.Screen
             var fullScreen = new ScreenSection(ScreenSectionPosition.Zero, new ScreenSectionData(8, 128));
             var blankExample = new ScreenSection(new ScreenSectionPosition(2, 25), new ScreenSectionData(1, 45));
             var lineExample = new ScreenSection(new ScreenSectionPosition(2, 65), new ScreenSectionData(3, 32));
+            var textExample = new ScreenSection(new ScreenSectionPosition(0, 0), new ScreenSectionData(2, 128));
             WithScreen(busAddress, screenSize, screen =>
             {
                 screen.Init();
                 ClearDisplay(screen, fullScreen);
-
-                PrintBlank(screen, blankExample);
-                PrintLineChain(screen, lineExample,
-                    new Point(0, 0),
-                    new Point(22, 5),
-                    new Point(4, 15),
-                    new Point(16, 12),
-                    new Point((int)lineExample.Data.Width - 1, (int)lineExample.Data.Height - 1),
-                    new Point(0, (int)lineExample.Data.Height - 1),
-                    new Point((int)lineExample.Data.Width - 1, 0),
-                    new Point(0, 0));
+                TryDifferentFonts(screen, textExample);
                 Console.ReadLine();
             });
+        }
+
+        private static void TryDifferentFonts(Ssd1306E2CScreen screen, ScreenSection section){
+            var fonts = Directory.GetFiles("/usr/share/fonts/", "*.otf", SearchOption.AllDirectories);
+            foreach(var font in fonts){
+                ClearDisplay(screen, section);
+                PrintBorder(screen, section);
+                PrintText(screen, section, "Hello World!", font);
+                Log.Info($"Font: '{font}'");
+                Console.ReadLine();
+            }
+        }
+
+        private static void PrintText(Ssd1306E2CScreen screen, ScreenSection section, string text, string fontPath)
+        {
+            var bmp = new Bitmap((int)section.Data.Width, (int)section.Data.Height);
+            var rect = new RectangleF(0, 0, bmp.Width, bmp.Height);
+            var format = new StringFormat(){
+                Alignment = StringAlignment.Near,
+                LineAlignment = StringAlignment.Near,
+                Trimming = StringTrimming.Character,
+            };
+            var graphics = Graphics.FromImage(bmp);
+            var fonts = new PrivateFontCollection();
+            fonts.AddFontFile(fontPath);
+            var fontFamily = fonts.Families.FirstOrDefault();
+            if (fontFamily is default(FontFamily))
+            {
+                throw new InvalidCastException("Cant find font");
+            }
+            
+            var font = new Font(fontFamily, bmp.Height/2);
+            graphics.DrawString(text, font, Brushes.Black, rect, format);
+            graphics.Flush();
+            
+            for (uint x = 0; x < rect.Width; x++)
+                for (uint y = 0; y < rect.Height; y++)
+                {
+                    var pixel = bmp.GetPixel((int)x, (int)y);
+                    if (pixel.A > byte.MaxValue * 0.1)
+                    {
+                        section.Data.SetPixel(x, y);
+                    }
+                }
+            screen.UpdateSection(section);
+        }
+
+        private static void PrintBorder(Ssd1306E2CScreen screen, ScreenSection section)
+        {
+            PrintLineChain(screen, section,
+                                new Point(0, 0),
+                                new Point((int)section.Data.Width - 1, 0),
+                                new Point((int)section.Data.Width - 1, (int)section.Data.Height - 1),
+                                new Point(0, (int)section.Data.Height - 1),
+                                new Point(0, 0));
         }
 
         private static void PrintLineChain(Ssd1306E2CScreen screen, ScreenSection section, params Point[] points)
@@ -54,7 +104,6 @@ namespace My.E2C.Screen
             {
                 var end = points[i];
                 PrintLine(screen, section, start, end);
-                Console.ReadLine();
                 start = end;
             }
         }
@@ -78,10 +127,10 @@ namespace My.E2C.Screen
                     var aI = a - i;
                     var cI = aI / angleA;
                     var bI = Math.Sqrt(cI * cI - aI * aI);
-                    Log.Info($"a: {aI};b:{bI};c:{cI}");
+                    
                     var x = p1.X + (isAPositive ? -aI : aI);
                     var y = p1.Y + (isBPositive ? -bI : bI);
-                    Log.Info($"x: {x};y:{y};");
+                    
                     section.Data.SetPixel((uint)x, (uint)y);
                 }
             }
@@ -92,10 +141,10 @@ namespace My.E2C.Screen
                     var bI = b - i;
                     var cI = bI / angleB;
                     var aI = Math.Sqrt(cI * cI - bI * bI);
-                    Log.Info($"a: {aI};b:{bI};c:{cI}");
+                    
                     var x = p1.X + (isAPositive ? -aI : aI);
                     var y = p1.Y + (isBPositive ? -bI : bI);
-                    Log.Info($"x: {x};y:{y};");
+                    
                     section.Data.SetPixel((uint)x, (uint)y);
                 }
             }
--- My.I2C.Screen/readme.md
+++ My.I2C.Screen/readme.md
@@ -1,7 +1,14 @@
 
 ## Info
+dependency:
+- libgdiplus
+
 
 Device IC: SS1315
 
 sudo i2cdetect -y 1
 https://www.raspberrypi-spy.co.uk/2018/04/i2c-oled-display-module-with-raspberry-pi/
+
+cd ./Projects/TryI2CMonitor
+dotnet build ./My.I2C.Screen/My.I2C.Screen.csproj 
+sudo ./My.I2C.Screen/bin/Debug/net6.0/My.I2C.Screen 1
